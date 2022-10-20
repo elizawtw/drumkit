0a330519c724ca3c4b6772fb91c16a3dac1af33f
eliza <twew02@gmail.com>
2022-10-20 12:43:12 -0400

    detect keyboard press

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
 
 
 
-
+//detecting button click
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
-
-    switch (buttonInnerHTML) {
-      case 'w':
-        const crash = new Audio("sounds/crash.mp3")
-        crash.play();
-        break;
-
-      case 'a':
-        const kick = new Audio("sounds/kick-bass.mp3")
-        kick.play();
-        break;
-
-      case 's':
-        const snare = new Audio("sounds/snare.mp3")
-        snare.play();
-        break;
-
-      case 'd':
-        const tom1 = new Audio("sounds/tom-1.mp3")
-        tom1.play();
-        break;
-
-      case 'j':
-        const tom2 = new Audio("sounds/tom-2.mp3")
-        tom2.play();
-        break;
-
-      case 'k':
-        const tom3 = new Audio("sounds/tom-3.mp3")
-        tom3.play();
-        break;
-
-      case 'l':
-        const tom4= new Audio("sounds/tom-4.mp3")
-        tom4.play();
-        break;
-
-      default: console.log(buttonInnerHTML)
-        break;
-    }
+    makeSound(buttonInnerHTML);
+    
   })
 }
 
+//detecing keyboard press
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key)
+})
+
+function makeSound(key) {
+
+  switch (key) {
+    case 'w':
+      const crash = new Audio("sounds/crash.mp3")
+      crash.play();
+      break;
+
+    case 'a':
+      const kick = new Audio("sounds/kick-bass.mp3")
+      kick.play();
+      break;
+
+    case 's':
+      const snare = new Audio("sounds/snare.mp3")
+      snare.play();
+      break;
+
+    case 'd':
+      const tom1 = new Audio("sounds/tom-1.mp3")
+      tom1.play();
+      break;
+
+    case 'j':
+      const tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play();
+      break;
+
+    case 'k':
+      const tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play();
+      break;
+
+    case 'l':
+      const tom4= new Audio("sounds/tom-4.mp3")
+      tom4.play();
+      break;
+
+    default: console.log(buttonInnerHTML)
+      break;
+  }
+}
